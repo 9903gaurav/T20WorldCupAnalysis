856f522050b38323dadf04a5cf338f1ccbb22b0c
Gaurav Jha <gaurav.200399@gmail.com>
2024-03-14 01:39:08 +0530

    format

--- web.py
+++ web.py
@@ -1,11 +1,10 @@
-url = 'https://stats.espncricinfo.com/ci/engine/records/team/match_results.html?id=14450;type=tournament'
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-import re
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+
+url = 'https://stats.espncricinfo.com/ci/engine/records/team/match_results.html?id=14450;type=tournament'
 
 service = Service(executable_path=r'/usr/lib/chromium-browser/chromedriver')
 
@@ -28,15 +27,13 @@ wait = WebDriverWait(driver, 10)
 
 rows = driver.find_elements(By.TAG_NAME, 'tr')
 
-# with open('dataset/match_summary1.csv', mode='w', newline='', encoding='utf-8') as file:
-#     writer = csv.writer(file)
 for row in rows:
     cols = row.find_elements(By.TAG_NAME, 'td')
     if cols:
         data = [col.text for col in cols]
         if cols[6].find_elements(By.XPATH, ".//a"):
-            match_links.append([cols[0].text,cols[1].text,cols[6].text,cols[6].find_element(By.XPATH, ".//a").get_attribute("href")])
-        # writer.writerow(data)
+            match_links.append([cols[0].text, cols[1].text, cols[6].text, cols[6].find_element(By.XPATH, ".//a").get_attribute("href")])
+
 print(match_links)
 
 driver.quit()
