9b5d45d4453b226dbb35c07b5602522be9124257
Gaurav Jha <gaurav.200399@gmail.com>
2024-03-13 13:38:10 +0530

    jjj

--- web.py
+++ web.py
@@ -19,14 +19,15 @@ wait = WebDriverWait(driver, 10)
 
 rows = driver.find_elements(By.TAG_NAME, 'tr')
 
-with open('dataset/match_summary1.csv', mode='w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
-    for row in rows:
-        cols = row.find_elements(By.TAG_NAME, 'td')
-        if cols:
-            data = [col.text for col in cols]
-            if cols[6].find_elements(By.XPATH, ".//a"):
-                match_links.append([cols[0].text,cols[1].text,cols[6].text,cols[6].find_element(By.XPATH, ".//a").get_attribute("href")])
-            writer.writerow(data)
+# with open('dataset/match_summary1.csv', mode='w', newline='', encoding='utf-8') as file:
+#     writer = csv.writer(file)
+for row in rows:
+    cols = row.find_elements(By.TAG_NAME, 'td')
+    if cols:
+        data = [col.text for col in cols]
+        if cols[6].find_elements(By.XPATH, ".//a"):
+            match_links.append([cols[0].text,cols[1].text,cols[6].text,cols[6].find_element(By.XPATH, ".//a").get_attribute("href")])
+        # writer.writerow(data)
+print(match_links)
 
 driver.quit()
